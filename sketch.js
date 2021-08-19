7677709b2ceeb283813b7cd0ea0a142acc6f04da
sprtygirl101 <86930639+sprtygirl101@users.noreply.github.com>
2021-08-19 15:37:58 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+if (keyisDown(DOWN_ARROW))
+{
+  background(red);
+}
+if (keyisUP(UP_ARROW))
+{
+   background(blue);
+}
+
+if(keyisLEFT(DOWN_ARROW)
+{
+  background(green);
+}
+if(keyisRIGHT(UP_ARROW)
+{
+  background(yellow);
+}
